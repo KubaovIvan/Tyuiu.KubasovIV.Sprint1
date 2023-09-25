eff322a7befc2656b850fec7ace3dc1cebeb9299
Tyuiu.KubasovIV@outlook.com <ubasovvanya@gmail.com>
2023-09-25 22:06:18 +0500

    Task4V9

---
+++ Tyuiu.KubasovIV.Sprint1.Task4.V9.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KubasovIV.Sprint1.Task4.V9.Lib;
+
+namespace Tyuiu.KubasovIV.Sprint1.Task4.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double y = 2;
+            double wait = 1.179;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.KubasovIV.Sprint1.Task4.V9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KubasovIV.Sprint1.Task4.V9.Lib;
+
+namespace Tyuiu.KubasovIV.Sprint1.Task4.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Кубасов И. В. | АСОиУБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Class Math                                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Кубасов Иван Васильевич | АСОиУБ-23-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            int x, y;
+
+            Console.WriteLine("Введите значение переменной x:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной y:");
+            y = Convert.ToInt32(Console.ReadLine());
+  
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("ln(x * y) / (x - sqrt (1 + y ^ 2) = " + ds.Calculate(x, y));
+            Console.ReadLine();
+        }
+    }
+}
